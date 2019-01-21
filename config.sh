773617367bce8a403598fa6082a168392f5aa7f0
MlgmXyysd <mlgmxyysd@meowcat.org>
2019-01-21 12:06:24 +0800

    v16

--- config.sh
+++ config.sh
@@ -15,13 +15,3 @@ REPLACE="
 set_permissions() {
   set_perm_recursive  $MODPATH  0  0  0755  0644
 }
-check_system() {
-  SYSTEM_VERSION=`grep_prop ro.build.version.sdk /system/build.prop`
-  if [[ "$SYSTEM_VERSION" != "28" ]]; then
-    ui_print "- Only support Pie based rom, current is $SYSTEM_VERSION ."
-    ui_print "- Aborting."
-    exit 1
-  else
-    ui_print "- Device Android Version: $SYSTEM_VERSION ."
-  fi
-}
